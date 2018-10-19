d5497dfc6cd5add8d87ed05c8aac1ad92378b0fb
ruian2 <ruian2@illinois.edu>
2018-10-18 20:30:47 -0400

    change structure

--- heart/cmake-build-debug/CMakeFiles/heart-size.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/heart-size"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/heart-size.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- heart/cmake-build-debug/compiler-file.d
+++
@@ -1,2 +0,0 @@
-compiler-file.o: \
- /private/var/folders/ph/08s6gfjj78vc3lwy92mg2f1h0000gn/T/compiler-file
