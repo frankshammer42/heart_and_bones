757add2eab8d2e389976af4b7c0ed3736e516f9d
ruian2 <ruian2@illinois.edu>
2018-10-21 14:43:29 -0400

    change the way to handle heatbeats

--- connection_test_radio/one_to_one/transmitter/transmitter.ino
+++ connection_test_radio/one_to_one/transmitter/transmitter.ino
@@ -1,5 +1,7 @@
-#include <VirtualWire.h>
 
+#include <VirtualWire.h>
+#define USE_ARDUINO_INTERRUPTS false    // Set-up low-level interrupts for most
+#include <PulseSensorPlayground.h>
 #define BPS 8000 // transmission rate (bits per second)
 
 // IDs of the receiver modules [1;255]
@@ -9,21 +11,64 @@
 #define MESSAGE_LENGTH 3 // packet size in bytes
 
 #define MILLIS_IDLE_BETWEEN_TRANSMISSION 9 // recommended approx. MESSAGE_LENGTH * 8 / BPS * 1,000 * 3
+#define DATA_PIN 12
+#define LED13 13
+#define PulseWire 0
 
 
+PulseSensorPlayground pulseSensor;  
 uint8_t outputBuffer[MESSAGE_LENGTH]; // output buffer
+bool transfered_data = false;
+int threshold = 550;
+int pulse_check_threshold = 8;
+int pulse_check_times = 0;
+int pulse_sum = 0;
+byte samplesUntilReport;
+const byte SAMPLES_PER_SERIAL_SAMPLE = 10;
 
-void setup() {
-  vw_set_tx_pin(12); // transmitter module data pin
-  vw_setup(BPS); // transmission rate
+
+void heart_pulse_setup(){
+  pulseSensor.analogInput(PulseWire);   
+  pulseSensor.blinkOnPulse(LED13);         
+  pulseSensor.setThreshold(threshold);   
+  pulseSensor.setSerial(Serial);
+  samplesUntilReport = SAMPLES_PER_SERIAL_SAMPLE;
+  if (pulseSensor.begin()) {
+    Serial.println("Pulse Sensor Initiated");  
+  }
 }
 
-void loop() {
-  sendValue(RECEIVER1_ID, 2309); // send data to receiver 1
-  /*sendValue(RECEIVER2_ID, 63); // send data to receiver 2*/
+/*void get_pulse(){*/
+  /*int myBPM = pulseSensor.getBeatsPerMinute();  */
+  /*Serial.println(myBPM);*/
+  /*if (pulseSensor.sawStartOfBeat()) {             */
+	  /*Serial.println("♥!!!"); */
+	  /*Serial.print("BPM: ");                         */
+	  /*Serial.println(myBPM);                         */
+	  /*pulse_check_times += 1;*/
+	  /*pulse_sum += myBPM;*/
+  /*}*/
+
+  /*//Test*/
+  /*delay(20);        */
+/*}*/
+
+
+bool get_enough_pulse(){
+  if (pulse_check_times == pulse_check_threshold){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
-void sendValue(int receiverId, int value) {
+void reset_pulse_params(){
+  pulse_check_times = 0;
+  pulse_sum = 0; 
+}
+
+void send_pulse(int receiverId, int value) {
   byte* valPtr = (byte*)&value;
   
   // fill output buffer with data
@@ -37,3 +82,45 @@ void sendValue(int receiverId, int value) {
   delay(MILLIS_IDLE_BETWEEN_TRANSMISSION); // wait some more time
 }
 
+void setup() {
+  Serial.begin(115200);
+  heart_pulse_setup(); 
+  vw_set_tx_pin(DATA_PIN); // transmitter module data pin
+  vw_setup(BPS); // transmission rate
+}
+
+void loop() {
+  int pulse_to_transfer = 0;
+  /*get_pulse();*/
+  if (pulseSensor.sawNewSample()) {
+    if (--samplesUntilReport == (byte) 0) {
+      samplesUntilReport = SAMPLES_PER_SERIAL_SAMPLE;
+      /*pulseSensor.outputSample();*/ // out put samples to check
+      if (pulseSensor.sawStartOfBeat()) {
+  		Serial.print("♥!!!"); 
+        Serial.print(" Get beats ----------");	
+		int myBPM = pulseSensor.getBeatsPerMinute();
+        Serial.println(myBPM);
+		pulse_check_times += 1;
+		pulse_sum += myBPM;
+      }
+    }
+    if (get_enough_pulse()){
+      int pulse_to_transfer = int(pulse_sum/pulse_check_times);
+      reset_pulse_params();
+      if (!transfered_data){
+        Serial.println("Start to send data");
+        send_pulse(RECEIVER1_ID , pulse_to_transfer);
+      }
+    }
+  }
+  
+  /*if (get_enough_pulse()){*/
+    /*int pulse_to_transfer = int(pulse_sum/pulse_check_times);*/
+    /*reset_pulse_params();*/
+    /*if (!transfered_data){*/
+      /*Serial.println("Start to send data");*/
+      /*send_pulse(RECEIVER1_ID , pulse_to_transfer);*/
+    /*}*/
+  /*}*/
+}
